d1f4d1fa35bf709896cafb9720a318e54695126a
Dace Liepina <dace.liepina77@gmail.com>
2025-11-26 21:39:34 +0100

    action removed

--- src/features/auth/authSlice.ts
+++ src/features/auth/authSlice.ts
@@ -41,8 +41,8 @@ export const authSlice = createSlice({
 				}
 				// в случае успешного входа уберем ошибку - если она была
 			})
-			.addCase(login.rejected, (state, action) => {
-				state.error = action.error.message || 'Wrong email or password' // случай неудачного входа - добавили ошибку
+			.addCase(login.rejected, (state) => {
+				state.error =  'Wrong email or password' // случай неудачного входа - добавили ошибку
 			})
 	},
 })
